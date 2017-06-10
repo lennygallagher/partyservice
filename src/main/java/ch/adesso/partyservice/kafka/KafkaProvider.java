786ac1676452828ed737887e3d7210c79f1cfdf5
aranda <aranda.maria@gmail.com>
2017-06-10 14:57:08 +0200

    Kafka Client added

---
+++ src/main/java/ch/adesso/partyservice/kafka/KafkaProvider.java
@@ -0,0 +1,53 @@
+package ch.adesso.partyservice.kafka;
+
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.clients.producer.KafkaProducer;
+
+import javax.annotation.PostConstruct;
+import javax.ws.rs.Produces;
+import java.util.Arrays;
+import java.util.Properties;
+import java.util.UUID;
+
+
+public class KafkaProvider {
+
+    public static final String TOPIC = "toDefine";
+    public static final String KAFKA_ADDRESS = "localhost";
+    public static final String GROUP_ID = "toDefine";
+
+    private KafkaProducer<String, String> producer;
+    private KafkaConsumer<String, String> consumer;
+
+    @PostConstruct
+    public void init() {
+        this.producer = createProducer();
+        this.consumer = createConsumer();
+    }
+
+    @Produces
+    public KafkaProducer<String, String> getProducer() {
+        return producer;
+    }
+
+    public KafkaProducer<String, String> createProducer() {
+        Properties properties = new Properties();
+        properties.put("bootstrap.servers", KAFKA_ADDRESS);
+        properties.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        properties.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        return new KafkaProducer<>(properties);
+    }
+
+    public KafkaConsumer<String, String> createConsumer() {
+        Properties properties = new Properties();
+        properties.put("bootstrap.servers", KAFKA_ADDRESS);
+        properties.put("group.id", GROUP_ID + UUID.randomUUID().toString());
+        properties.put("auto.offset.reset", "earliest");
+        properties.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+        properties.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+
+        KafkaConsumer<String, String> consumer = new KafkaConsumer<>(properties);
+        consumer.subscribe(Arrays.asList(TOPIC));
+        return consumer;
+    }
+}
