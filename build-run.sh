2bc1fa7ec13e2663507a27853f18c5fbd060c353
lennygallagher <lenny.gallagher@gmail.com>
2017-06-12 17:25:04 +0200

    typo

--- build-run.sh
+++ build-run.sh
@@ -5,4 +5,4 @@ docker build -t maria/partyservice:${1} .
 docker stop partyservice
 docker rm partyservice
 docker run -d --name partyservice --net=hackathon -p 8081:8080 maria/partyservice:${1}
-docker logs partservice -f
+docker logs partyservice -f
