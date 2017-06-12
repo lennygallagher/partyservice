33b471022e2457cdeda6646ee18e54066e14045d
lennygallagher <lenny.gallagher@gmail.com>
2017-06-12 14:56:57 +0200

    name added

--- src/main/java/ch/adesso/partyservice/party/boundary/PartyResource.java
+++ src/main/java/ch/adesso/partyservice/party/boundary/PartyResource.java
@@ -5,12 +5,17 @@ import ch.adesso.partyservice.party.entity.Person;
 import ch.adesso.partyservice.party.entity.PersonCreatedEvent;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.RecordMetadata;
 
 import javax.ejb.Stateless;
 import javax.inject.Inject;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import java.util.UUID;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
 
 @Path("parties")
 @Produces(MediaType.APPLICATION_JSON)
@@ -22,13 +27,16 @@ public class PartyResource {
     private KafkaProducer<String,String> producer;
 
     @POST
-    public Person createPerson(Person person) {
+    public Person createPerson(Person person) throws InterruptedException, ExecutionException, TimeoutException {
         UUID uuid = UUID.randomUUID();
         person.setId(uuid.toString());
         ProducerRecord<String, String> event = new ProducerRecord<>(KafkaProvider.TOPIC,
                 person.getId(), new PersonCreatedEvent(person.getId()).toJson().toString());
-        producer.send(event);
-        System.out.println("Person: " + person.toString());
+        Future<RecordMetadata> md = producer.send(event);
+        RecordMetadata rnd = md.get(1l, TimeUnit.SECONDS);
+        rnd.offset();
+        System.out.println("Person: " + person.toString() + " and offset: " + rnd.offset());
+        person.setFirstname("Maria: " + rnd.offset());
         return person;
     }
 
